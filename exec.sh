8429604c91b3de0bfab25af18c37cecd1dea3098
ildyria <beviguier@gmail.com>
2023-04-30 16:38:49 +0200

    fix exec.sh

--- exec.sh
+++
@@ -1,5 +0,0 @@
-# ansible-playbook playbook.yml -K --tags "terminal,fonts,development,browser,images,gui" -vv
-# ansible-playbook playbook.yml -K --tags "terminal,gui,discord" -vv
-ansible-playbook playbook.yml -K --tags "gui" -vv
-
-# ansible-playbook playbook.yml --list-tasks
